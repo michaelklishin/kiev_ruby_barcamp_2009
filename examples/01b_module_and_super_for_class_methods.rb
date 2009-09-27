c07138b1212295008f738417729b80557241109c
Michael S. Klishin <michael@novemberain.com>
2009-09-27 16:59:54 +0300

    Annotate example 01b: super call from modules perfectly works for class object methods

--- examples/01b_module_and_super_for_class_methods.rb
+++ examples/01b_module_and_super_for_class_methods.rb
@@ -1,5 +1,18 @@
 #!/usr/bin/env ruby -Ku
 
+#
+# Example #1 for class object methods (some would call them "class methods").
+# super call from module methods works exactly the same.
+#
+# In this example, we override .find to change it's behavior a tad: it first goes
+# to Memcached to fetch results. Original behavior (load from the database)
+# is used as a fallback.
+#
+# This module can be included into any class that needs this behavior. Nice, eaasy
+# and transparent.
+#
+# And, exception traces will be readable.
+
 module WriteThroughCache
   module ClassMethods
     def find(*args)
@@ -13,7 +26,7 @@ module WriteThroughCache
   end
 
   module InstanceMethods
-    
+
   end
 
   def self.included(receiver)
